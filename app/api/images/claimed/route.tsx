436df7850f2f4fd9f3f90b3e743c1bbc0ecf209a
Jaxcoder <codenamejason@outlook.com>
2024-02-10 00:21:32 -0500

    smol updates

--- app/api/images/claimed/route.tsx
+++ app/api/images/claimed/route.tsx
@@ -4,7 +4,7 @@ import { CARD_DIMENSIONS } from '../../../config';
 
 export async function GET() {
   return new ImageResponse(
-    <Card message="You've already claimed, but you can mint with Warps." />,
+    <Card message="You've already donated, are you sure you want to donate again?" />,
     CARD_DIMENSIONS,
   );
 }
--- contracts/zora-minter/script/Deploy.s.sol
+++ contracts/zora-minter/script/Deploy.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.23;
+pragma solidity 0.8.20;
 
 import {ZoraMinter} from "../src/ZoraMinter.sol";
 import {Script, console} from "forge-std/Script.sol";
